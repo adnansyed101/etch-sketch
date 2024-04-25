9934e9841ea3b64c417aaff5560d8f334d5c42d4
Md Adnan <sirgeant10@gmail.com>
2024-04-25 19:31:35 +0600

    Create the grid.
    The main function will now create the initial grid on 16X16.

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+const boxContainer = document.querySelector(".box-container");
+
+function main(blockNum) {
+  const flexBasis = 100 / blockNum;
+  for (let i = 1; i <= blockNum ** 2; i++) {
+    const box = document.createElement("div");
+    box.classList = "box";
+    boxContainer.appendChild(box);
+    box.style.flexBasis = `${flexBasis}%`;
+  }
+}
+
+main(16);
