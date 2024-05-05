addcffb9b4d6f7638f1aa4a7fe20f85a278f6bc1
Md Adnan <sirgeant10@gmail.com>
2024-05-05 13:22:36 +0600

    Add random color to each boxes/
    At first the btn node has been selected. Later a boolean variable of 'isRandomColor' has been taken. On click of the button the value will be changed to true or false. And random colors will be created as inline css. This random color is created from in "line 23". And is applied in "line 26".

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const boxContainer = document.querySelector(".box-container");
 let numOfBox = prompt("Enter number of boxes within 2 and 100");
 const createGridBtn = document.querySelector("#createGridBtn");
+const randomColorBtn = document.querySelector("#randomizeColorBtn");
+let isRandomColor = false;
 
 createGridBtn.addEventListener("click", () => {
   numOfBox = prompt("Enter number of boxes within 2 and 100");
+  isRandomColor = false;
+  main();
+});
+
+randomColorBtn.addEventListener("click", () => {
+  isRandomColor = !isRandomColor;
   main();
 });
 
@@ -12,8 +20,10 @@ function createGrid(blockNum) {
   for (let i = 0; i < blockNum; i++) {
     const rowDiv = document.createElement("div");
     for (let j = 0; j < blockNum; j++) {
+      let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
       const box = document.createElement("div");
       box.classList = "box";
+      box.style.backgroundColor = isRandomColor ? randomColor : "";
       rowDiv.append(box);
     }
     boxContainer.appendChild(rowDiv);
